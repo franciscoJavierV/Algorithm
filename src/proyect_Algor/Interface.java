f2f9059a44346afeb7a33f01caa47112e5517b24
franciscoJavierV <franjavi4111@gmail.com>
2018-11-26 13:19:09 -0500

    lol

--- src/proyect_Algor/Interface.java
+++ src/proyect_Algor/Interface.java
@@ -1,5 +1,2 @@
 package proyect_Algor;
 
-public interface Interface {
-
-}
--- src/proyect_Algor/Main.java
+++ src/proyect_Algor/Main.java
@@ -1,5 +1,18 @@
 package proyect_Algor;
 
-public class Main {
+import java.io.BufferedReader; 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
+public class Main {
+	public static void main(String[] args) {
+	Modelo mo = new Modelo();
+	String ruta = "txt/commandsyo.txt";
+	mo.leer();			
+	}		
 }
